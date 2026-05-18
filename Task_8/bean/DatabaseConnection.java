af3417b4968f36f51b72d822ab93c6f62f3c0a91
srigurudharshini123 <srigurudharshini@gmail.com>
2026-05-18 20:49:05 +0530

    Create DatabaseConnection.java

---
+++ Task_8/bean/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package bean;
+
+public class DatabaseConnection {
+ public void start() {
+	System.out.println("Init Method stated!....");
+}
+public void connet() {
+	System.out.println("connected successfully!....");
+}
+
+public void end() {
+	System.out.println("destroy Method stated!....");
+}
+
+}
